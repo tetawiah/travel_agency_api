b8e4a599fb317c03f2030b3b45112e9ff9d56e67
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2023-06-10 22:52:58 +0000

    added unique and uuid to model,added accessor

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -2,12 +2,40 @@
 
 namespace App\Models;
 
+use Attribute;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Travel extends Model
 {
+    protected $table = 'travels';
     protected $primaryKey = 'uuid';
+    public $timestamps = false;
 
     use HasFactory;
+    use HasSlug;
+    use HasUuids;
+
+    protected $fillable = ['is_public', 'slug', 'name', 'description', 'num_of_days',];
+
+    public function tours()
+    {
+        return $this->hasMany(Tour::class);
+    }
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    public function getNumOfNightsAttribute()
+    {
+        return $this->num_of_days - 1;
+    }
+
 }
---
+++ database/migrations/2023_06_09_220943_create_travels_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->uuid()->primary();
+            $table->boolean('is_public')->default(false);
+            $table->string('slug')->unique();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('num_of_days');
+          
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('travels');
+    }
+};
