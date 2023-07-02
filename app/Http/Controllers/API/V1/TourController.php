349c27d64f6a250c8f9786e370a0e753dad32fbd
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2023-07-02 23:40:49 +0000

    added filters and sort

--- app/Http/Controllers/API/V1/TourController.php
+++ app/Http/Controllers/API/V1/TourController.php
@@ -4,13 +4,30 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TourResource;
+use App\Models\Tour;
 use App\Models\Travel;
+use Illuminate\Support\Facades\DB;
 use Ramsey\Collection\Collection;
 
 class TourController extends Controller
 {
     public function index(Travel $travel)
     {
-        return TourResource::collection( $travel->tours()->orderBy('start_date')->paginate());
+        $request = request();
+           $tours =  $travel->tours()
+                ->when($request->filled('priceFrom') || $request->filled('priceTo'), function ($query) use ($request) {
+                    return $query->where('price','>=',$request->priceFrom)
+                        ->where('price','<=',$request->priceTo);
+            })
+                ->when($request->filled('startDate') || $request->filled('endDate'),function ($query) use ($request) {
+                    return $query->where('start_date','>=',$request->startDate)
+                        ->where('end_date', '<=', $request->endDate);
+                })
+               ->when($request->filled('sortby') && $request->filled('sortOrder'),function ($query) use ($request) {
+                   return $query->orderby($request->sortby,$request->sortOrder);
+               })
+                ->get();
+
+        return TourResource::collection($tours);
     }
 }
